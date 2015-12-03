e13ff6c51adb287615b614357decd934ebe90d7e
blablupcom <o.volodin@yahoo.com>
2015-12-03 22:32:02 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -86,7 +86,7 @@ def convert_mth_strings ( mth_string ):
 
 entity_id = "E4603_DMBC_gov"
 url = 'http://www.dudley.gov.uk/resident/your-council/local-transparency/council-expenditure-over-500'
-proxy = urllib2.ProxyHandler({'http': '176.227.202.26:9574'})
+proxy = urllib2.ProxyHandler({'http': '178.62.75.42:3128'})
 opener = urllib2.build_opener(proxy)
 urllib2.install_opener(opener)
 errors = 0
